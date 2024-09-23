5989429a2d8767e287d412ccbb2ce224a3fe1cb9
hustcer <hustcer@outlook.com>
2024-09-23 11:39:00 +0800

    Try to create cross build image from silkeh/clang:18

--- rust-cross-freebsd/setup-cross-compile.sh
+++ rust-cross-freebsd/setup-cross-compile.sh
@@ -11,15 +11,15 @@ apt-get update
 apt-get install -y --no-install-recommends ${BUILD_DEPENDENCIES}
 
 # Create cross compile wrapper scripts for clang.
-cat >> "/usr/local/bin/${CLANG_TRIPLE}-clang" <<EOF
+cat >> "/usr/bin/${CLANG_TRIPLE}-clang" <<EOF
 #!/bin/sh
-/usr/local/bin/clang --sysroot=${FREEBSD_BASE} "\$@" --target=${CLANG_TRIPLE}
+/usr/bin/clang --sysroot=${FREEBSD_BASE} "\$@" --target=${CLANG_TRIPLE}
 EOF
-cat >> "/usr/local/bin/${CLANG_TRIPLE}-clang++" <<EOF
+cat >> "/usr/bin/${CLANG_TRIPLE}-clang++" <<EOF
 #!/bin/sh
-/usr/local/bin/clang++ --sysroot=${FREEBSD_BASE} "\$@" --target=${CLANG_TRIPLE}
+/usr/bin/clang++ --sysroot=${FREEBSD_BASE} "\$@" --target=${CLANG_TRIPLE}
 EOF
-chmod 755 "/usr/local/bin/${CLANG_TRIPLE}-clang" "/usr/local/bin/${CLANG_TRIPLE}-clang++"
+chmod 755 "/usr/bin/${CLANG_TRIPLE}-clang" "/usr/bin/${CLANG_TRIPLE}-clang++"
 
 groupadd -r rust -g 2000
 useradd -m -r -g rust -u 2000 rust
@@ -38,18 +38,21 @@ su rust -c ". /home/rust/.cargo/env; rustup target add ${RUSTC_TRIPLE}"
 # Set the linker for Cargo.
 su rust -c "cat >> /home/rust/.cargo/config.toml <<EOF
 [target.${RUSTC_TRIPLE}]
-linker = \"/usr/local/bin/${CLANG_TRIPLE}-clang\"
+linker = \"/usr/bin/${CLANG_TRIPLE}-clang\"
 EOF"
 
 # Script to set environment variables needed for cross compiling. This is useful to allow for running unit tests on
 # the host before targeting the cross environment.
 su rust -c "cat >> /home/rust/set-cross-compile-env.sh <<EOF
-export CC_$(echo "${RUSTC_TRIPLE}" | sed s/-/_/g)=/usr/local/bin/${CLANG_TRIPLE}-clang
-export CXX_$(echo "${RUSTC_TRIPLE}" | sed s/-/_/g)=/usr/local/bin/${CLANG_TRIPLE}-clang++
+export CC_$(echo "${RUSTC_TRIPLE}" | sed s/-/_/g)=/usr/bin/${CLANG_TRIPLE}-clang
+export CXX_$(echo "${RUSTC_TRIPLE}" | sed s/-/_/g)=/usr/bin/${CLANG_TRIPLE}-clang++
 EOF"
 
 chmod 777 /home/rust/.cargo
 chmod 777 /home/rust/.rustup
+
+rm /usr/bin/ld
+ln -s /usr/bin/ld.lld /usr/bin/ld
 
 # Cleanup
 # shellcheck disable=SC2086
